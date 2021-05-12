03cbc9c0a809ea6faa3125fd51678787af3a59aa
bartimus <null>
2021-05-12 10:44:42 -0400

    Added constants

--- main/tag.c
+++ main/tag.c
@@ -4,9 +4,10 @@
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_CONSTANT);
-    writeChunk(&chunk, addConstant(&chunk, 232.2));
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, addConstant(&chunk, 232.2), 1);
+    writeChunk(&chunk, OP_RETURN, 1);
+    writeChunk(&chunk, OP_RETURN, 3);
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
